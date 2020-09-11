86be28d448eadf8094b57f8347f2a68aae13bcf0
Diogo Marques <dimarques@gmail.com>
2020-09-11 11:54:50 -0300

    Primeiro projeto React alura ceep

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "./estilo.css";
+class FormularioCadastro extends Component {
+
+  constructor(props){
+    super(props);
+    this.titulo="";
+    this.texto="";
+  }
+  // o underline antes, é uma conveção para mostrar que são privados
+  _handleMudancaTitulo(evento) {
+   this.titulo=evento.target.value;
+   
+  }
+
+  _handleMudancaTexto(evento){
+    this.texto= evento.target.value;
+  }
+
+  criarNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto);
+   
+  }
+
+  render() {
+    return (
+      <form className="form-cadastro "
+      onSubmit={this.criarNota.bind(this)}
+      >
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this._handleMudancaTitulo.bind(this)}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+          onChange={this._handleMudancaTexto.bind(this)}
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
+  }
+}
+
+export default FormularioCadastro;
